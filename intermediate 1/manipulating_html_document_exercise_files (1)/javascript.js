b206e4eb002393bc7c20a86fa4b27b86a4ee5105
Johannes Zarre <zarre.johannes@gmail.com>
2020-10-07 13:35:33 +0200

    intermediate

--- intermediate 1/manipulating_html_document_exercise_files (1)/javascript.js
+++ intermediate 1/manipulating_html_document_exercise_files (1)/javascript.js
@@ -4,13 +4,21 @@ var json = JSON.parse(products);
 
 $(document).ready(function(){
 
-    for(let i=0; i < products.length; i++){
-        let test = $(`#${i}`).find("span").text();
-        console.log(test);
+    for(let i=0; i < json.length; i++){
+        $(`#${i} span:contains('Product Category')`).text(json[i].type);
+        
+        if (json[i].type == "Notebook"){
+            $(`#${i}`).css("background-color", "red");
+        } else if (json[i].type == "Phone"){
+        $(`#${i}`).css("background-color", "green");
+        }
+        else {
+            $(`#${i}`).css("background-color", "blue");
+        }
 
     };
 
-//    let a =  $(".name").find("span")
+
 
 
 })
