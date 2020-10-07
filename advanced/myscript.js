7f7b0acebc171f8543589a61d257de55143e21d0
Johannes Zarre <zarre.johannes@gmail.com>
2020-10-07 15:09:44 +0200

    finished advanced

--- advanced/myscript.js
+++ advanced/myscript.js
@@ -5,8 +5,26 @@ $(document).mousemove(function(event){
     let top = `${event.pageX - 128}px`;
     let left = `${event.pageY - 128}px`;
 
-    $("#moveme").css({"top": left,"left":top})
+    $("#moveme").css({"top": left,"left":top});
   })
 
+  $("#moveme").one("click",function(event){
+    let top = `${event.pageX - 128}px`;
+    let left = `${event.pageY - 128}px`; 
 
-});
+    
+    $("#moveme").attr("id", "moveme1");
+    // $("#moveme1").css({"top": left,"left":top});
+    $("#moveme1").animate({
+        opacity: 1,
+        paddingTop: '+=800',
+    });
+    $("#moveme1").fadeOut(2000);
+    $("#moveme1").fadeIn(2000);
+
+
+
+  });
+
+
+}); 
